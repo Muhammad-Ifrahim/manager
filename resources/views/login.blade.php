a8d47fabdc684ce8ad20e6fafdd2b725f35a7f5e
ifrahim <ifrahimafzal.awan@gmail.com>
2018-08-05 14:00:49 +0500

    code-indentation

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,59 +1,52 @@
 @extends('layouts.login-master')
+<style type="text/css">
+body{
+background: grey;
+}
 
-    <style type="text/css">
-          body{
-            background: grey;  
-          } 
-           
-    </style>
+</style>
 @section('content')
 
-   
-   <div class="container" style="margin-top:50%;">    
-        <div id="loginbox"  class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
-            <div class="panel panel-info" >
-                    <div class="panel-heading">
-                        <div class="panel-title">Sign In</div>
-                        <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Forgot password?</a></div>
-                    </div>     
-
-                    <div style="padding-top:30px" class="panel-body" >
-
-                            
-                 @include('common.errors')
-                               
-       {!! Form::open(['url' => 'login',  'method' => 'POST', 'class' => 'form-horizontal']) !!}
-           
-           <div class="form-group {{ $errors->has('Name') ? 'has-error' : ''}} ">
-                      {!! Form::label('Name', 'Name:', ['class' => 'col-lg-2 control-label']) !!}
-              <div class="col-lg-10">
-                  {!! Form::text('Name', $value = null, ['class' => 'form-control', 'placeholder' => 'Name']) !!}
-                 <div class="help-block">{{ $errors->first('Name') }}</div>
-            </div>
+<div class="container" style="margin-top:50%;">
+  <div id="loginbox"  class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+    <div class="panel panel-info" >
+      <div class="panel-heading">
+        <div class="panel-title">Sign In</div>
+        <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Forgot password?</a></div>
+      </div>
+      <div style="padding-top:30px" class="panel-body" >
+        
+        @include('common.errors')
+        
+        {!! Form::open(['url' => 'login',  'method' => 'POST', 'class' => 'form-horizontal']) !!}
+        
+        <div class="form-group {{ $errors->has('Name') ? 'has-error' : ''}} ">
+          {!! Form::label('Name', 'Name:', ['class' => 'col-lg-2 control-label']) !!}
+          <div class="col-lg-10">
+            {!! Form::text('Name', $value = null, ['class' => 'form-control', 'placeholder' => 'Name']) !!}
+            <div class="help-block">{{ $errors->first('Name') }}</div>
           </div>
-
-
-        
-
-       <div class="form-group {{ $errors->has('Password') ? 'has-error' : '' }} ">
-           {!!Form::label('Password','Password:',['class' => 'col-lg-2 control-label' ]) !!}
-           <div class="col-lg-10">
-              {!! Form::text('Password', $value=null, ['class' => 'form-control','placeholder' => 'Password'])!!}
-               <div class="help-block">{{ $errors->first('Code') }}</div>
-           </div>
-       </div>
-      
-      
-        <div class="form-group">
-            <div class="col-lg-10 col-lg-offset-2">
-                {!! Form::submit('Submit', ['class' => 'btn btn-lg btn-info pull-right'] ) !!}
-            </div>
         </div>
-                                
         
-         {!! Form::close()  !!} 
-         </div>                     
-      </div>  
+        <div class="form-group {{ $errors->has('Password') ? 'has-error' : '' }} ">
+          {!!Form::label('Password','Password:',['class' => 'col-lg-2 control-label' ]) !!}
+          <div class="col-lg-10">
+            {!! Form::text('Password', $value=null, ['class' => 'form-control','placeholder' => 'Password'])!!}
+            <div class="help-block">{{ $errors->first('Code') }}</div>
+          </div>
+        </div>
+        
+        
+        <div class="form-group">
+          <div class="col-lg-10 col-lg-offset-2">
+            {!! Form::submit('Submit', ['class' => 'btn btn-lg btn-info pull-right'] ) !!}
+          </div>
+        </div>
+        
+        
+        {!! Form::close()  !!}
+      </div>
     </div>
-    
-@endSection('content')
+  </div>
+  
+  @endSection('content')
--- resources/views/proforma/proforma-view.blade.php
+++ resources/views/proforma/proforma-view.blade.php
@@ -23,7 +23,6 @@ margin: 0 10px;
 <section class="content">
   <div class="row">
     <div class="col-md-12">
-      
       <div class="box">
         <div class="box-header">
           <h3 class="box-title">Proforma</h3>
